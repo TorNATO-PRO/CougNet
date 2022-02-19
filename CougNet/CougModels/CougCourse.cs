06892e309d5a116bdd57b1cee2e0a5bab67ed9b8
funsooje <funsooje@gmail.com>
2022-02-19 11:51:15 -0800

    Base Model classes
    New Class library:
    Add Coug, CougCourse, CougYear, Gender and Program classes.

---
+++ CougNet/CougModels/CougCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CougModels
+{
+    public class CougCourse
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        //possibly create faculty for this
+    }
+}

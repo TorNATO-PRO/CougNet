1e83a435882a3802c7f3e95ca780d4c8db724dd4
funsooje <funsooje@gmail.com>
2022-02-19 14:48:31 -0800

    CougProfile

--- CougNet/CougNet/Controllers/CougsController.cs
+++ CougNet/CougNet/Controllers/CougsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,11 +21,15 @@ namespace CougNet
 
         // GET: Cougs
         public async Task<IActionResult> Index()
-        {
-            var cougID = User.Identity.Name;
+        { 
+            var appID = User.Identity.Name;
 
             // Check if the person has a profile already
-            var cougProfile = _context.Coug.Where(x => x.AppId == cougID).FirstOrDefault();
+            var cougProfile = _context.Coug
+                .Include(x => x.Gender)
+                .Include(x => x.Year)
+                .Include(x => x.Major)
+                .Where(x => x.AppId == appID).FirstOrDefault();
 
             if (cougProfile == null)
             {

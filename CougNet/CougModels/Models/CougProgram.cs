afe1c7e0a096b1eb68a484b6e1da8fb6d0f0b14f
funsooje <funsooje@gmail.com>
2022-02-19 16:24:39 -0800

    Added requirements

--- CougNet/CougModels/Models/CougProgram.cs
+++ CougNet/CougModels/Models/CougProgram.cs
@@ -7,10 +7,14 @@ namespace CougModels
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        public string Requirements { get; set; }
 
         [Display(Name = "Approval Required?")]
         public bool ApprovalRequired { get; set; }

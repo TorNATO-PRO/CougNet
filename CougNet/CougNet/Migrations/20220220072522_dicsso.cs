1b4a7713fcf964e03f8688883092daa21d717061
funsooje <funsooje@gmail.com>
2022-02-19 23:31:50 -0800

    update

---
+++ CougNet/CougNet/Migrations/20220220072522_dicsso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CougNet.Migrations
+{
+    public partial class dicsso : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostDateTime",
+                table: "Discussions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostDateTime",
+                table: "Discussions");
+        }
+    }
+}

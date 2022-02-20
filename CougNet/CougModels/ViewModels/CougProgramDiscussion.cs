ecc0a5301f8f452fd734df81f7475766e1f005d1
funsooje <funsooje@gmail.com>
2022-02-19 20:59:31 -0800

    Create CougProgramDiscussion.cs

---
+++ CougNet/CougModels/ViewModels/CougProgramDiscussion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CougModels.ViewModels
+{
+    public class CougProgramDiscussion
+    {
+        public int CougProgramId { get; internal set; }
+
+        public int parentID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int CougID { get; set; }
+
+    }
+}
